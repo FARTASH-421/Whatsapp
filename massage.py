32d0b0893955113e36c5b39b8a9abc2a23cd2037
Fartash-421 <aq.fartash.421@outlook.com>
2024-04-21 00:14:25 +0330

    Initial commit

---
+++ massage.py
@@ -0,0 +1,4 @@
+data = b'\x01\x02\x03\x04\x05'
+
+with open('file.hex', 'wb') as f:
+    f.write(data)
---
+++ whatsapp.py
@@ -0,0 +1,14 @@
+import pywhatkit
+import time
+
+
+
+
+min = int(18)
+
+# while min < 55:
+
+pywhatkit.sendwhatmsg("+989926067519", "Hi", 21, min,30)
+    # time.sleep(60)
+
+# Send a WhatsApp Message to a Contact at 1:30 PM
